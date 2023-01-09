53f3256a59a32ea21095f2e68f69a27893dde25c
sharekim <sksyag@naver.com>
2023-01-09 22:44:45 +0900

    logo 적용

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => {
@@ -6,7 +7,7 @@ const Header = () => {
     <>
       <Head>
         <title>양수교회</title>
-        <meta name="description" content="양수교회 홈페이지" />
+        <meta name="description" content="양수교회에 오신 것을 환영합니다" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
@@ -15,7 +16,7 @@ const Header = () => {
           href="/"
           className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
         >
-          로고위치
+          <Image src="/logo.png" width={250} height={100} alt="logo" />
         </Link>
 
         <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
