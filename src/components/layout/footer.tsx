6134b6c0cd43d96f3a985f56ca4f0a6fac070cff
Sharekim <nnkim@bixolon.com>
2023-01-09 16:24:47 +0900

    footer 추가

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-3 my-4">
+      <p className="text-center text-muted">
+        (우)476-823 경기도 양평군 양서면 북한강로 29
+      </p>
+      <p className="text-center text-muted">
+        TEL : 031-772-6161~6162 / FAX : 031-772-6161
+      </p>
+      <p className="text-center text-muted">
+        Copyright 2023 Yangsu Church. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
-import Header from "../components/header";
+import Header from "../components/layout/header";
+import Footer from "../components/layout/footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
