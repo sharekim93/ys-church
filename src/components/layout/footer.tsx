40540fe97065bdf731d5667aae7a3f084a544249
sharekim <sksyag@naver.com>
2023-01-09 22:45:08 +0900

    footer 전화번호 표기 변경

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         (우)476-823 경기도 양평군 양서면 북한강로 29
       </p>
       <p className="text-center text-muted">
-        TEL : 031-772-6161~6162 / FAX : 031-772-6161
+        TEL : 031-772-6161~2 / FAX : 031-772-6161
       </p>
       <p className="text-center text-muted">
         Copyright 2023 Yangsu Church. All rights reserved.
