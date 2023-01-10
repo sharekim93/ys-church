00d34390941a2917836a60d9d3bddd6f2cf69ac8
Sharekim <nnkim@bixolon.com>
2023-01-10 16:52:29 +0900

    샘플소스 제거 및 실제 표어 이미지 추가

--- src/components/mainCarousel/mainCarousel.tsx
+++ src/components/mainCarousel/mainCarousel.tsx
@@ -1,32 +1,14 @@
 import Image from "next/image";
-import { Carousel } from "react-bootstrap";
+import Carousel from "react-bootstrap/Carousel";
 
 const mainCarousel = () => {
   return (
     <Carousel fade>
       <Carousel.Item>
         <Image
-          className="d-block w-100"
-          src="/vercel.svg"
+          className="d-block"
+          src="/gd_y202311.jpg"
           alt="First slide"
-          width={590}
-          height={398}
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image
-          className="d-block w-100"
-          src="/next.svg"
-          alt="Second slide"
-          width={590}
-          height={398}
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image
-          className="d-block w-100"
-          src="/thirteen.svg"
-          alt="Third slide"
           width={590}
           height={398}
         />
