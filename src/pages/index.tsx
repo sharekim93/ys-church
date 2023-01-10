b5ee0e927b834ba30a353518c8a66e4ccb5fa78a
Sharekim <nnkim@bixolon.com>
2023-01-10 16:52:51 +0900

    index 페이지에 접근성을 위한 태그 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
-import { Inter } from "@next/font/google";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { MainCarousel } from "../components/mainCarousel";
-import styles from "../styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <main className={styles.main}>
-        <MainCarousel />
+      <main>
+        <h1 className="visually-hidden">메인화면</h1>
+        <Container>
+          <h2 className="visually-hidden">소개 및 표어 이미지</h2>
+          <Row>
+            <Col>
+              <h3 className="visually-hidden">양수교회 소개글</h3>
+              <p className="lead text-start">양수교회에 오신 것을 환영합니다</p>
+            </Col>
+            <Col>
+              <h3 className="visually-hidden">표어 이미지</h3>
+              <MainCarousel />
+            </Col>
+          </Row>
+        </Container>
       </main>
     </>
   );
