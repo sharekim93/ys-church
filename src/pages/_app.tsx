04ab6c51a611ba229eb125f4cc4fd9612f0f4d29
Sharekim <nnkim@bixolon.com>
2023-01-11 16:57:25 +0900

    Noto Sans KR 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
-import "../styles/globals.css";
+import "../styles/globals.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
 import Header from "../components/layout/header";
 import Footer from "../components/layout/footer";
+import { Noto_Sans_KR } from "@next/font/google";
+
+const noto = Noto_Sans_KR({
+  subsets: ["korean"],
+  display: "swap",
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <div className="container">
+      <div className={`container-fluid vh-100 gx-0 ${noto.className}`}>
         <Header />
         <Component {...pageProps} />
         <Footer />
