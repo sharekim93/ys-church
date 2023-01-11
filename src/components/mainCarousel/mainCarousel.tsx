204299ef0f36c04c4abd8c98fbcf871d8818b67c
Sharekim <nnkim@bixolon.com>
2023-01-11 16:58:37 +0900

    반응형으로 Carousel 적용

--- src/components/mainCarousel/mainCarousel.tsx
+++ src/components/mainCarousel/mainCarousel.tsx
@@ -3,14 +3,19 @@ import Carousel from "react-bootstrap/Carousel";
 
 const mainCarousel = () => {
   return (
-    <Carousel fade>
+    <Carousel fade className="w-100">
       <Carousel.Item>
         <Image
           className="d-block"
           src="/gd_y202311.jpg"
-          alt="First slide"
+          alt="banner"
           width={590}
           height={398}
+          sizes="100vw"
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
         />
       </Carousel.Item>
     </Carousel>
