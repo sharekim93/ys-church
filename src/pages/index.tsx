115aec7e06bc4eb1149192e385767279c7b2b1e4
sharekim <sksyag@naver.com>
2023-01-11 21:51:52 +0900

    예배 아이콘 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { MainCarousel } from "../components/mainCarousel";
+import { FaPlaceOfWorship } from "react-icons/fa";
 
 import { Nanum_Pen_Script } from "@next/font/google";
 
@@ -21,10 +22,15 @@ export default function Home() {
           <MainCarousel />
         </Container>
         <Container>
-          <h2 className="visually-hidden">예배시간</h2>
-          <Row className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between mt-4 py-4 bg-primary">
-            <Col className="text-center">Col 1</Col>
-            <Col className="text-center">Col 2</Col>
+          <h2 className="text-center mt-4 py-4 bg-primary">예배시간 안내</h2>
+          <Row className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between mt-4 py-4">
+            <Col className="text-center">
+              <FaPlaceOfWorship />
+              <p className="">주일예배</p>
+            </Col>
+            <Col className="text-center">
+              <p className="">수요예배</p>
+            </Col>
             <Col className="text-center">Col 3</Col>
             <Col className="text-center">Col 4</Col>
           </Row>
