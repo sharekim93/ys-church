d467652c2a116564ab6655745f6f174c95a1c07f
sharekim <sksyag@naver.com>
2023-01-11 21:28:16 +0900

    헤더 메뉴 간격 조정

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between px-4 py-3 mb-4 border-bottom bg-light">
+      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between px-4 px-2 mb-4 border-bottom bg-light">
         <Link
           href="/"
           className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
@@ -22,22 +22,22 @@ const Header = () => {
 
         <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
           <li>
-            <Link href="#" className="nav-link px-2 link-dark">
+            <Link href="#" className="nav-link px-4 link-dark">
               <p className={styles.linkBtn}>교회소개</p>
             </Link>
           </li>
           <li>
-            <Link href="#" className="nav-link px-2 link-dark">
+            <Link href="#" className="nav-link px-4 link-dark">
               <p className={styles.linkBtn}>교회소식</p>
             </Link>
           </li>
           <li>
-            <Link href="#" className="nav-link px-2 link-dark">
+            <Link href="#" className="nav-link px-4 link-dark">
               <p className={styles.linkBtn}>교회앨범</p>
             </Link>
           </li>
           <li>
-            <Link href="#" className="nav-link px-2 link-dark">
+            <Link href="#" className="nav-link px-4 link-dark">
               <p className={styles.linkBtn}>오시는길</p>
             </Link>
           </li>
